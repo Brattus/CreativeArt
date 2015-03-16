1915560ab4a6be93ee32229bbb6d61f27b69b0f4
Ramin <ramin_esfandiari_93@hotmail.com>
2015-03-16 20:42:28 +0100

    Funker fett :D

---
+++ src/DisplayFrame.java
@@ -0,0 +1,143 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Ramin on 16.03.2015.
+ */
+public class DisplayFrame extends JFrame
+{
+    private RandomArt randomArtProcessing;
+
+    private JPanel processingPanel;
+
+    private JButton triangleButton;
+    private JButton squareButton;
+    private JButton circleButton;
+    private JButton colorPicker;
+    private JButton resetButton;
+
+    private JLabel title;
+
+
+    public static void main(String[] args)
+    {
+        DisplayFrame displayFrame = new DisplayFrame("Random Art Generator");
+    }
+
+
+    /**
+     * Construct a new DisplayFrame
+     * @throws HeadlessException
+     */
+    public DisplayFrame(String title) throws HeadlessException
+    {
+        this.setTitle( title );
+        initiateFrame();
+        initiateComponents();
+
+        addComponents();
+
+        setLayout();
+
+        addActionListeners();
+
+
+    }
+
+
+    /**
+     * Initiate the Frame configurations
+     */
+    private void initiateFrame()
+    {
+        this.setSize(1366 , 768 );
+        this.setMinimumSize( new Dimension( 1366, 768 ) );
+        if(this != null)
+        {
+            this.setResizable( true );
+        }
+        setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
+        setLocationRelativeTo( null );
+
+        setVisible( true );
+    }
+
+
+    /**
+     * Initiate the field components
+     */
+    private void initiateComponents()
+    {
+        randomArtProcessing = new RandomArt();
+        randomArtProcessing.init();
+        processingPanel = new JPanel();
+
+        triangleButton = new JButton( "Triangles" );
+        squareButton = new JButton("Squares");
+        circleButton = new JButton( "Circles" );
+        resetButton = new JButton( "Clear Canvas" );
+        colorPicker = new JButton( "Color Picker" );
+
+        title = new JLabel( "Random Art Generator" );
+        title.setForeground( new Color( 0, 0, 0 ) );
+        title.setFont( new Font( null, 2, 25 ) );
+    }
+
+
+    /**
+     * Add the components to the frame
+     */
+    private void addComponents()
+    {
+        this.add( triangleButton );
+        this.add( processingPanel );
+        this.add( squareButton );
+        this.add( circleButton );
+        this.add( resetButton );
+        this.add( colorPicker );
+
+        this.add( title );
+
+        processingPanel.add( randomArtProcessing );
+    }
+
+
+    /**
+     * Set the layout for the components
+     */
+    private void setLayout()
+    {
+        setLayout( null );
+
+        processingPanel.setBounds( getWidth()-1030, 10, 1000, getHeight()-10 );
+
+
+        triangleButton.setBounds( 10, 100, 100, 25 );
+        squareButton.setBounds( 120, 100, 100, 25 );
+        circleButton.setBounds( 230, 100, 100, 25 );
+        colorPicker.setBounds( 10, 200, 120, 25 );
+        resetButton.setBounds( 10, getHeight()-80, 150, 25 );
+
+        title.setBounds( 10, 10, 300, 100 );
+    }
+
+    private void addActionListeners()
+    {
+        triangleButton.addActionListener( randomArtProcessing );
+        triangleButton.setActionCommand( "triangles" );
+
+        squareButton.addActionListener( randomArtProcessing );
+        squareButton.setActionCommand( "squares" );
+
+        circleButton.addActionListener( randomArtProcessing );
+        circleButton.setActionCommand( "circles" );
+
+        resetButton.addActionListener( randomArtProcessing );
+        resetButton.setActionCommand( "clear" );
+
+        colorPicker.addActionListener( randomArtProcessing );
+        colorPicker.setActionCommand( "color" );
+    }
+}
+
+
---
+++ src/RandomArt.java
@@ -0,0 +1,163 @@
+/**
+ * Created by Ramin on 16.03.2015.
+ */
+
+import processing.core.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class RandomArt extends PApplet implements ActionListener
+{
+    // Need G4P library
+
+    PGraphics pg1;
+    PGraphics pg2;
+    PImage img;
+
+    Gui gui;
+
+    //fonts
+    PFont myFont;
+    PFont myFont1;
+
+    int st;
+    PGraphics pGraphics;
+
+    public void setup(){
+
+        size(1000, 700);
+
+        background(0);
+
+        noStroke();
+    }
+
+    @Override
+    public void draw()
+    {
+    }
+
+
+    public void gui(){
+        pg1.beginDraw();
+        pg1.background(200);
+        image(pg1,displayWidth-displayWidth/4,displayHeight-displayHeight);
+        pg1.endDraw();
+
+        //Setting the font, texts size and text of the right window.
+        myFont = createFont("Arial", 25);
+        textFont(myFont);
+        textAlign(CENTER, CENTER);
+        fill(255);
+        String s = "Welcome to Random Art Generator!";
+        fill(255);
+        text(s, displayWidth-displayWidth/4, displayHeight/40, displayWidth/4, displayHeight/12);  // Text wraps within text box
+
+        //Font text
+        textAlign(LEFT,CENTER);
+        textSize(22);
+        text("Choose filter:", (float) (displayWidth-displayWidth/4.1), (float) (displayHeight/2.55), 400,100);
+
+
+        //Objects text
+        textAlign(LEFT,CENTER);
+        textSize(22);
+        text("Choose Objects:", (float) (displayWidth-displayWidth/4.1), (float) (displayHeight/3.8), 400,100);
+
+    }
+
+    // Use this method to add additional statements
+    // to customise the GUI controls
+
+    void imageChosen( File f )
+    {
+        if( f.exists() )
+        {
+            img = loadImage( f.getAbsolutePath() );
+        }
+    }
+
+
+    /**
+     * Creates random triangles
+     */
+    public void createTriangles()
+    {
+        println( "Triangle clicked at: " + millis()/1000 + " s" );
+        for(st = 0; st < 50; st++)
+        {
+           fill( random( 255 ), random( 255 ), random( 255 ), 200 );
+            triangle( random ( 0, width ), random( 0, height ), random( 0, (float) ( width / 1.7 ) ), random( 0, height ), (float) ( width / 2 ), height / 2 );
+        }
+    }
+
+    /**
+     * Creates random squares
+     */
+    public void createSquares()
+    {
+        println("Square clicked at: " + millis());
+        for(st = 0; st < 50; st++){
+            noStroke ();
+            fill (random (255), random (255), random(255), 170);
+            rect(random(0,width), random(0,height), 60,60);
+        }
+    }
+
+    /**
+     * Creates random circles
+     */
+    public void createCircles()
+    {
+        println("Circle clicked at: " + millis());
+        for(st = 0; st < 50; st++){
+            noStroke ();
+            fill (random (255), random (255), random(255), 170);
+            ellipse (random (0, width), random (0, height), 60, 60);
+        }
+    }
+
+    public void clearCanvas()
+    {
+        clear();
+    }
+
+        private void changeColor()
+        {
+            Color c = JColorChooser.showDialog(null, "Choose a Color", Color.BLACK);
+             background( c.getRGB() );
+        }
+
+
+
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e
+     */
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        switch(e.getActionCommand())
+        {
+            case "triangles":
+                createTriangles();
+                break;
+            case "squares":
+                createSquares();
+                break;
+            case "circles":
+                createCircles();
+                break;
+            case "clear":
+                clearCanvas();
+                break;
+            case "color":
+                changeColor();
+        }
+    }
+}
